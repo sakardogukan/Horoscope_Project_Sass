c9e199414fa23ecf4ecb7e792581f1bebdbf7a0c
DGKN <sakardogukan@gmail.com>
2024-04-08 23:43:47 +0300

    project files repo updated

---
+++ src/compenents/main/Card.jsx
@@ -0,0 +1,20 @@
+const Card = (props) => {
+  console.log("ne geliyor : ", props)
+  const { title, date, image, desc } = props
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+      <div className="date">
+        <h2>{date}</h2>
+      </div>
+      <img src={image} alt="" />
+      <div className="card-over">
+        <p>{desc}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
